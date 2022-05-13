97914f0b49a77f8aadb6469d668c051c8e002ca5
100Gyeon <bjy9966@naver.com>
2022-05-14 02:56:04 +0900

    feat: 정확도 50% 이상일 때만 결과 보이도록 설정

--- frontend/src/pages/Refrigerator.jsx
+++ frontend/src/pages/Refrigerator.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import styled from 'styled-components';
 import * as tmImage from '@teachablemachine/image';
 
 function Refrigerator() {
@@ -7,7 +8,6 @@ function Refrigerator() {
   const metadataURL = URL + 'metadata.json';
 
   const [file, setFile] = useState('');
-  const [result, setResult] = useState('');
   const [loading, setLoading] = useState(false);
   const [ingredientList, setIngredientList] = useState([]);
 
@@ -17,9 +17,8 @@ function Refrigerator() {
     const prediction = await model.predict(tempImage, false);
 
     prediction.sort((a, b) => parseFloat(b.probability) - parseFloat(a.probability));
-    setIngredientList(prediction);
+    setIngredientList(prediction.filter((ingredient) => ingredient.probability * 100 > 50));
     setLoading(false);
-    setResult(prediction[0].className);
   };
 
   const handleChange = (e) => {
@@ -28,7 +27,6 @@ function Refrigerator() {
 
     setLoading(true);
     setIngredientList([]);
-    setResult('');
 
     if (targetFile) {
       reader.onload = () => setFile(reader.result.toString());
@@ -38,21 +36,55 @@ function Refrigerator() {
   };
 
   return (
-    <div>
-      <h1>냉장고 안에 있는 재료를 찾아볼까요?</h1>
+    <StyledRefrigerator>
+      <h1>
+        냉장고 안에 있는
+        <br />
+        재료를 찾아볼까요?
+      </h1>
       <div>
         <input onChange={handleChange} type="file" accept="image/*" />
-        {file && <img id="userImage" src={file} />}
+        {file && <StyledUserImage id="userImage" src={file} />}
       </div>
-      {loading && <div>결과 로딩 중</div>}
-      {result && (
-        <>
-          <div>{ingredientList[0].className}</div>
-          <div>{`${(ingredientList[0].probability * 100).toFixed(1)}%`}</div>
-        </>
+      {loading ? (
+        <div>결과 로딩 중</div>
+      ) : (
+        <StyledResult>
+          {ingredientList.map(({ className, probability }) => (
+            <div key={className}>
+              <span>{className}</span>
+              <span>{`${(probability * 100).toFixed(1)}%`}</span>
+            </div>
+          ))}
+        </StyledResult>
       )}
-    </div>
+    </StyledRefrigerator>
   );
 }
 
 export default Refrigerator;
+
+const StyledRefrigerator = styled.div`
+  min-height: calc(100vh - 8rem);
+
+  h1 {
+    font-size: 2rem;
+    line-height: 120%;
+    margin-bottom: 2rem;
+  }
+`;
+
+const StyledUserImage = styled.img`
+  width: 100%;
+  margin-bottom: 2rem;
+`;
+
+const StyledResult = styled.div`
+  font-size: 1.4rem;
+  div + div {
+    margin-top: 1rem;
+  }
+  span + span {
+    margin-left: 0.5rem;
+  }
+`;
