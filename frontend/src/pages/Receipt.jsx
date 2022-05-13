516a6f6d4d96b59b54605ab9fc7ed02d91fac343
100Gyeon <bjy9966@naver.com>
2022-05-14 04:26:24 +0900

    feat: tesseract.js 활용해서 이미지에서 글자 추출하기

--- frontend/src/pages/Receipt.jsx
+++ frontend/src/pages/Receipt.jsx
@@ -1,5 +1,48 @@
+import { useState, useCallback, useEffect } from 'react';
+import { createWorker } from 'tesseract.js';
+
 function Receipt() {
-  return <div>영수증</div>;
+  const worker = createWorker();
+  const [receiptImage, setReceiptImage] = useState(null);
+  const [textResult, setTextResult] = useState('');
+
+  const convertImageToText = useCallback(async () => {
+    if (!receiptImage) return;
+    await worker.load();
+    await worker.loadLanguage('eng+kor');
+    await worker.initialize('eng+kor');
+    const { data } = await worker.recognize(receiptImage);
+    setTextResult(data.lines);
+  }, [receiptImage, worker]);
+
+  useEffect(() => {
+    if (receiptImage) {
+      convertImageToText();
+    }
+  }, [receiptImage]);
+
+  const handleReceiptImage = (e) => {
+    if (e.target.files[0]) {
+      setReceiptImage(e.target.files[0]);
+    } else {
+      setReceiptImage(null);
+      setTextResult('');
+    }
+  };
+
+  return (
+    <div>
+      <h1>영수증에서 재료를 찾아볼까요?</h1>
+      <input type="file" accept="image/*" onChange={handleReceiptImage} />
+      <div>
+        {receiptImage && <img src={URL.createObjectURL(receiptImage)} alt="영수증" />}
+        {textResult &&
+          textResult.map((line, idx) => {
+            return <div key={idx}>{line.text}</div>;
+          })}
+      </div>
+    </div>
+  );
 }
 
 export default Receipt;
