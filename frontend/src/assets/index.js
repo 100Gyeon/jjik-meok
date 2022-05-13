fde81090e759ae9ef06555b18bc454eacecdaa20
100Gyeon <bjy9966@naver.com>
2022-05-14 01:28:29 +0900

    chore: 필요한 asset 추가

---
+++ frontend/src/assets/index.js
@@ -0,0 +1,3 @@
+export { default as icRefrigerator } from './icons/refrigerator.png';
+export { default as icReceipt } from './icons/receipt.png';
+export { default as lottieChoice } from './lottie/choice.json';
