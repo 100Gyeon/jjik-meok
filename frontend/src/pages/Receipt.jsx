d74be0988d274776268d327b7e15554a7d17e113
100Gyeon <bjy9966@naver.com>
2022-05-21 03:05:40 +0900

    feat: 크롭한 이미지에서 한글 추출하도록 구현

--- frontend/src/pages/Receipt.jsx
+++ frontend/src/pages/Receipt.jsx
@@ -1,45 +1,50 @@
-import { useState, useCallback, useEffect } from 'react';
-import styled from 'styled-components';
+import { useState, useRef } from 'react';
 import { createWorker } from 'tesseract.js';
+import Cropper from 'react-cropper';
+import 'cropperjs/dist/cropper.css';
+import styled from 'styled-components';
 
 function Receipt() {
   const worker = createWorker();
+  const cropperRef = useRef(null);
   const [receiptImage, setReceiptImage] = useState(null);
+  const [croppedImage, setCroppedImage] = useState(null);
   const [textResult, setTextResult] = useState('');
 
-  const convertImageToText = useCallback(async () => {
-    if (!receiptImage) return;
+  const convertImageToText = async () => {
+    if (!croppedImage) return;
     await worker.load();
     await worker.loadLanguage('kor');
     await worker.initialize('kor');
-    const { data } = await worker.recognize(receiptImage);
+    const { data } = await worker.recognize(croppedImage);
     setTextResult(data.lines);
-  }, [receiptImage, worker]);
-
-  useEffect(() => {
-    if (receiptImage) convertImageToText();
-  }, [receiptImage]);
+  };
 
   const handleReceiptImage = (e) => {
     if (e.target.files[0]) {
-      setReceiptImage(e.target.files[0]);
+      setReceiptImage(URL.createObjectURL(e.target.files[0]));
     } else {
       setReceiptImage(null);
       setTextResult('');
     }
   };
 
+  const onCrop = () => {
+    const imageElement = cropperRef?.current;
+    const cropper = imageElement?.cropper;
+    setCroppedImage(cropper.getCroppedCanvas().toDataURL());
+  };
+
   return (
     <StyledReceipt>
       <h1>영수증에서 재료를 찾아볼까요?</h1>
-      <input type="file" accept="image/*" onChange={handleReceiptImage} />
-      <div>
-        {receiptImage && <img src={URL.createObjectURL(receiptImage)} alt="영수증" />}
-        {textResult &&
-          textResult.map((line, idx) => {
-            return <StyledResult key={idx}>{line.text}</StyledResult>;
-          })}
-      </div>
+      <input type="file" accept="image/*" onChange={(e) => handleReceiptImage(e)} />
+      {receiptImage && <Cropper src={receiptImage} crop={onCrop} ref={cropperRef} />}
+      {croppedImage && <button onClick={() => convertImageToText()}>이미지 자르기</button>}
+      {textResult &&
+        textResult.map((line, idx) => {
+          return <StyledResult key={idx}>{line.text.replace(/[0-9]/g, '').replace(/\s/g, '')}</StyledResult>;
+        })}
     </StyledReceipt>
   );
 }
@@ -47,6 +52,7 @@ function Receipt() {
 export default Receipt;
 
 const StyledReceipt = styled.div`
+  width: 100%;
   min-height: calc(100vh - 8rem);
 
   h1 {
@@ -58,6 +64,16 @@ const StyledReceipt = styled.div`
   img {
     width: 100%;
     margin-bottom: 2rem;
+  }
+
+  button {
+    margin: 2rem 0;
+    width: 100%;
+    padding: 1.8rem 0;
+    border-radius: 1.6rem;
+    background-color: #0064ff;
+    color: #fff;
+    font-size: 1.4rem;
   }
 `;
 
